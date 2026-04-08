f26f6a0fce18114f09af898bb936eafb3ccb8924
Gerd Reiss <gerd@reiss.pro>
2026-04-08 11:09:16 +0200

    use local storage to store tasks

--- 01-essentials/src/app/tasks/tasks.service.ts
+++ 01-essentials/src/app/tasks/tasks.service.ts
@@ -8,6 +8,14 @@ import { type Task } from './task/task.model';
 export class TasksService {
   private tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -20,6 +28,7 @@ export class TasksService {
       summary: task.summary,
       dueDate: task.dueDate,
     });
+    this.saveTasks();
   }
 
   updateTask(updated: Task) {
@@ -30,9 +39,15 @@ export class TasksService {
         task.dueDate = updated.dueDate;
       }
     }
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
