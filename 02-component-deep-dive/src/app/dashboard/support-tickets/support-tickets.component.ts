9b8e18d691a00217af1545d7a939b525d2bddc78
Gerd Reiss <gerd@reiss.pro>
2026-04-09 15:56:54 +0200

    component deep dive - custom button component

--- 02-component-deep-dive/src/app/dashboard/support-tickets/support-tickets.component.ts
+++ 02-component-deep-dive/src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-support-tickets',
   templateUrl: './support-tickets.component.html',
   styleUrl: './support-tickets.component.css',
+  imports: [NewTicketComponent],
 })
 export class SupportTicketsComponent {}
