accb2894c1a6fcbdab2707345827a4808407b056
Gerd Reiss <gerd@reiss.pro>
2026-04-15 16:07:05 +0200

    custom two-way binding - impl

--- 02-custom-2w-binding/src/app/rect/rect.component.ts
+++ 02-custom-2w-binding/src/app/rect/rect.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-rect',
-    imports: [],
-    templateUrl: './rect.component.html',
-    styleUrl: './rect.component.css'
+  selector: 'app-rect',
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Todo: Implement custom two-way binding
+  // @Input({ required: true }) size!: { width: string; height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // size = input.required<{ width: string; height: string }>();
+  // sizeChange = output<{ width: string; height: string }>();
+  size = model.required<{ width: string; height: string }>();
 
   onReset() {
-    // ...
+    // this.sizeChange.emit({ width: '100', height: '100' });
+    this.size.set({ width: '100', height: '100' });
   }
 }
