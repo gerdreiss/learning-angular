d4b4ddfc0a04881f34a2173b6ab521f818c99caa
Gerd Reiss <gerd@reiss.pro>
2026-04-17 12:31:29 +0200

    services & di deep dive - create and inject service

---
+++ 09-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, signal } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+import { Task } from './task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  tasks = signal<Task[]>([]);
+
+  addTask(data: { title: string; description: string }) {
+    this.tasks.update((old) => [
+      ...old,
+      { ...data, id: uuidv4(), status: 'OPEN' },
+    ]);
+  }
+}
