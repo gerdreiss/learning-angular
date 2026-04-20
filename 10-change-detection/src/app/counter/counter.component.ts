a6d7e933bb18c5c3c66e59d3ea26b88177e7aedc
Gerd Reiss <gerd@reiss.pro>
2026-04-20 12:57:56 +0200

    change detection - initial commit

---
+++ 10-change-detection/src/app/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component, signal } from '@angular/core';
+
+import { InfoMessageComponent } from '../info-message/info-message.component';
+
+@Component({
+    selector: 'app-counter',
+    templateUrl: './counter.component.html',
+    styleUrl: './counter.component.css',
+    imports: [InfoMessageComponent]
+})
+export class CounterComponent {
+  count = signal(0);
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  onDecrement() {
+    this.count.update((prevCount) => prevCount - 1);
+  }
+
+  onIncrement() {
+    this.count.update((prevCount) => prevCount + 1);
+  }
+}
