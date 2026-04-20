0efdebf95a80dd6e2366befde87c2a38f0b49c1a
Gerd Reiss <gerd@reiss.pro>
2026-04-20 13:22:21 +0200

    change detection - avoiding zone pollution

--- 10-change-detection/src/app/counter/counter.component.ts
+++ 10-change-detection/src/app/counter/counter.component.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
 @Component({
-    selector: 'app-counter',
-    templateUrl: './counter.component.html',
-    styleUrl: './counter.component.css',
-    imports: [InfoMessageComponent]
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+  imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
+
   count = signal(0);
+
+  ngOnInit(): void {
+    setTimeout(() => this.count.set(0), 4000);
+
+    this.zone.runOutsideAngular(() =>
+      setTimeout(() => console.log('Timer expired!'), 5000),
+    );
+  }
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
