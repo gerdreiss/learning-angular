64f1a90c61e7ed08ed7a3b6d4feb9a4d94fe75e0
Gerd Reiss <gerd@reiss.pro>
2026-04-22 08:57:34 +0200

    change detection - onPush + rxjs

---
+++ 10-change-detection/src/app/messages/messages.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessagesService {
+  messages$ = new BehaviorSubject<string[]>([]);
+
+  private messages: string[] = [];
+
+  get aallMessages() {
+    return [...this.messages];
+  }
+
+  addMessage(message: string) {
+    this.messages = [...this.messages, message];
+    this.messages$.next([...this.messages]);
+  }
+}
--- 10-change-detection/src/app/messages/new-message/new-message.component.ts
+++ 10-change-detection/src/app/messages/new-message/new-message.component.ts
@@ -1,15 +1,17 @@
-import { Component, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MessagesService } from '../messages.service';
 
 @Component({
-    selector: 'app-new-message',
-    imports: [FormsModule],
-    templateUrl: './new-message.component.html',
-    styleUrl: './new-message.component.css'
+  selector: 'app-new-message',
+  imports: [FormsModule],
+  templateUrl: './new-message.component.html',
+  styleUrl: './new-message.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewMessageComponent {
-  add = output<string>();
-  enteredText = signal('');
+  private messageService = inject(MessagesService);
+  enteredText = '';
 
   get debugOutput() {
     console.log('[NewMessage] "debugOutput" binding re-evaluated.');
@@ -17,7 +19,7 @@ export class NewMessageComponent {
   }
 
   onSubmit() {
-    this.add.emit(this.enteredText());
-    this.enteredText.set('');
+    this.messageService.addMessage(this.enteredText);
+    this.enteredText = '';
   }
 }
