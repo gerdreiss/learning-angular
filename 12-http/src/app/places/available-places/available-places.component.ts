efa94defb27bf0f25040bd9f3aa803a25d4abd03
Gerd Reiss <gerd@reiss.pro>
2026-04-23 13:16:23 +0200

    http - loading data from backend

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -1,15 +1,39 @@
-import { Component, signal } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
 import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
 
 @Component({
-    selector: 'app-available-places',
-    templateUrl: './available-places.component.html',
-    styleUrl: './available-places.component.css',
-    imports: [PlacesComponent, PlacesContainerComponent]
+  selector: 'app-available-places',
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((data) => data.places),
+        catchError((error) =>
+          throwError(() => new Error('Something went wrong...')),
+        ),
+      )
+      .subscribe({
+        next: (places) => this.places.set(places),
+        complete: () => this.isFetching.set(false),
+        error: (err: Error) => this.error.set(err.message),
+      });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
--- 12-http/src/main.ts
+++ 12-http/src/main.ts
@@ -1,8 +1,8 @@
+import { provideHttpClient } from '@angular/common/http';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideZonelessChangeDetection()],
+  providers: [provideZonelessChangeDetection(), provideHttpClient()],
 }).catch((err) => console.error(err));
