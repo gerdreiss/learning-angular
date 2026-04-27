c564117c2094c29efb8f03e69bbc796ce7de50be
Gerd Reiss <gerd@reiss.pro>
2026-04-27 14:52:47 +0200

    routing - add first routes

---
+++ 14-routing/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+export const routes: Routes = [
+  { path: '', component: NoTaskComponent },
+  { path: 'tasks', component: TasksComponent },
+];
