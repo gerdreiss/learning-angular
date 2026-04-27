be95b9cef43d76dc2464f8b4f04d5d8e686bac27
Gerd Reiss <gerd@reiss.pro>
2026-04-27 17:26:44 +0200

    routing - dynamic routes

--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent /*implements OnInit*/ {
+  private usersService = inject(UsersService);
+
+  userId = input.required<string>(); // <-- this comes from the dynamic routes path parameter :userId
+  user = computed(() =>
+    this.usersService.users.find((u) => u.id === this.userId()),
+  );
+
+  // private activatedRouteService = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
+  // user = signal<User | undefined>(undefined);
+  // ngOnInit(): void {
+  //   const subscription = this.activatedRouteService.paramMap.subscribe({
+  //     next: (paramMap) =>
+  //       this.user.set(
+  //         this.usersService.users //
+  //           .find((u) => u.id === paramMap.get('userId')),
+  //       ),
+  //   });
+
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
+}
