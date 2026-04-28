b7ed73d7aec4ff295fcdf8a744a75883f3614cb1
Gerd Reiss <gerd@reiss.pro>
2026-04-28 16:39:42 +0200

    routing - fallback component

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -14,4 +15,5 @@ export const routes: Routes = [
       { path: 'tasks/new', component: NewTaskComponent },
     ],
   },
+  { path: '**', component: NotFoundComponent },
 ];
