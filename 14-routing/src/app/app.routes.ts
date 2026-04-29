c148e657adaf13e686fa2f8bd18e1f1329a84ed7
Gerd Reiss <gerd@reiss.pro>
2026-04-29 08:28:42 +0200

    routing - splitting routes into multiple files

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,20 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
   { path: '', component: NoTaskComponent },
-  {
-    path: 'users/:userId',
-    component: UserTasksComponent,
-    children: [
-      { path: '', redirectTo: 'paths', pathMatch: 'prefix' },
-      { path: 'tasks', component: TasksComponent },
-      { path: 'tasks/new', component: NewTaskComponent },
-    ],
-  },
+  { path: 'users/:userId', component: UserTasksComponent, children: userRoutes },
   { path: '**', component: NotFoundComponent },
 ];
